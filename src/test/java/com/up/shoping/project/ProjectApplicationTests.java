00b2ab63af191fd9c39bfef8a75b656f5e27449f
Yumer Chalashkan <71348719+ocaliskan@users.noreply.github.com>
2023-01-22 08:01:06 +0300

    first init

---
+++ src/test/java/com/up/shoping/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.up.shoping.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
