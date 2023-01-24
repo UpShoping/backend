c42f0a93b056aea04a1b4be1fa3e1b0cefd4f4b1
Mehmet Ari <105050927+kofthe@users.noreply.github.com>
2023-01-24 22:20:28 +0300

    update annotation
    update annotation

--- src/main/java/com/up/shoping/project/domain/product/Product.java
+++ src/main/java/com/up/shoping/project/domain/product/Product.java
@@ -1,5 +1,12 @@
 package com.up.shoping.project.domain.product;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     private Long id;
